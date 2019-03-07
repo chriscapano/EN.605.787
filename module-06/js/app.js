52ef02f1ec3951eadf4abd7c9b905b707291b5bc
Christopher Capano <chriscapano@gmail.com>
2019-03-06 23:07:29 -0500

    Module 6 Homework Complete

--- module-06/js/app.js
+++ module-06/js/app.js
@@ -20,12 +20,24 @@
             } else {
                 $scope.orderValid = true;
                 var lunchItems = $scope.lunchOrder.split(',');
-                if (lunchItems.length <= 3) {
+                var reducedItems = lunchItems.reduce(reduceCallBack, {valid: 0, empty: 0});
+
+                if (reducedItems.valid <= 3) {
                     $scope.checkerMessage = "Enjoy!";
                 } else {
                     $scope.checkerMessage = "Too Much!";
                 }
             }
+        };
+
+        function reduceCallBack(accumulator, currentValue) {
+            if (currentValue.trim() === "") {
+                accumulator.empty = accumulator.empty + 1;
+            } else {
+                accumulator.valid = accumulator.valid + 1;
+            }
+
+            return accumulator;
         }
     }
 })();
