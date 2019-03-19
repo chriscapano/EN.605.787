fe72e48642fc22a71d604d319c62e11c00c194a2
Christopher Capano <chriscapano@gmail.com>
2019-03-18 23:18:31 -0400

    mod-7 coursera portion

--- module-07/js/app.js
+++ module-07/js/app.js
@@ -1,4 +1,56 @@
 (function () {
     'use strict';
 
+    angular.module('ShoppingListCheckOff', [])
+        .controller('ToBuyController', ToBuyController)
+        .controller('AlreadyBoughtController', AlreadyBoughtController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+    ToBuyController.$inject = ['$scope', 'ShoppingListCheckOffService'];
+    function ToBuyController($scope, ShoppingListCheckOffService) {
+        var ctrl = this;
+
+        ctrl.items = ShoppingListCheckOffService.getItemsToBuy();
+
+        ctrl.buyItem = function (itemIndex) {
+            ShoppingListCheckOffService.buyItem(itemIndex);
+        }
+    }
+
+    AlreadyBoughtController.$inject = ['$scope', 'ShoppingListCheckOffService'];
+    function AlreadyBoughtController($scope, ShoppingListCheckOffService) {
+        var ctrl = this;
+
+        ctrl.items = ShoppingListCheckOffService.getItemsBought();
+    }
+
+    ShoppingListCheckOffService.$inject = [];
+    function ShoppingListCheckOffService() {
+        var defaultBuyList = [
+            { name: "cookies", quantity: 10 },
+            { name: "ice cream", quantity: 3 },
+            { name: "milk", quantity: 2 },
+            { name: "napkins", quantity: 20 },
+            { name: "whipped cream", quantity: 5 }
+        ];
+
+        var service = this;
+        var itemsToBuy = defaultBuyList;
+        var itemsBought = [];
+
+        service.buyItem = function (itemIndex) {
+            console.log("itemIndex: " + itemIndex);
+            itemsBought.push(itemsToBuy[itemIndex]);
+            itemsToBuy.splice(itemIndex, 1);
+        };
+
+        service.getItemsToBuy = function () {
+            return itemsToBuy;
+        };
+
+        service.getItemsBought = function () {
+            return itemsBought;
+        };
+    }
+
 })();
